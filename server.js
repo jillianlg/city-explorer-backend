7c308c1b347044d985ec5a0d9fc111a124aae181
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-09 16:27:13 -0800

    fix formating error

--- server.js
+++ server.js
@@ -5,49 +5,56 @@ const cors = require('cors');
 const request = require('superagent');
 const app = express();
 const port = 3000;
-const { mungeLocation } = require('.utils.js');
+const { mungeLocation } = require('utils.js');
 
 app.use(cors());
 
 // Location API
 app.get('/location', async(req, res) => {
-  const URL = 'https://us1.locationiq.com/v1/search.php?key={api-key}&q={city-name}&format=json';
+  try {
+    const URL = 'https://us1.locationiq.com/v1/search.php?key={api-key}&q={city-name}&format=json';
 
-  const response = await request.get(URL);
-  res.json(response.body);
+    const response = await request.get(URL);
+    const locRes = mungeLocation(response.body);
+
+    res.json(locRes);
+  } catch(e) {
+    res.json({ error: e.message });
+  }
 });
 
 // Weather API
-app.get('/weather', async(req, res) => {
-  const URL = 'https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543&key={api-key}';
+// app.get('/weather', async (req, res) => {
+//   const URL = 'https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543&key={api-key}';
 
-  const response = await request.get(URL);
-  res.json(response.body);
-});
+//   const response = await request.get(URL);
+//   res.json(response.body);
+// });
 
 // Yelp API
-app.get('/yelp', async(req, res) => {
-  const URL = 'https://api.yelp.com/v3/businesses/search?latitude={lat}&longitude={lng}';
+// app.get('/yelp', async (req, res) => {
+//   const URL = 'https://api.yelp.com/v3/businesses/search?latitude={lat}&longitude={lng}';
 
-  const response = await request.get(URL);
-  res.json(response.body);
-});
+//   const response = await request.get(URL);
+//   res.json(response.body);
+// });
 
 // Hiking API
-  app.get('/hiking', async(req, res) => {
-    const URL = 'https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lng}&maxDistance=200&key={api-key}';
+// app.get('/hiking', async (req, res) => {
+//   try {
+//     const URL = 'https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lng}&maxDistance=200&key={api-key}';
 
-    const response = await request.get(URL);
+//     const response = await request.get(URL);
 
-    const locRes = mungeLocation(response.body);
+//     const locRes = mungeLocation(response.body);
 
-      res.json(locRes);
-  } catch (e) {
-      res.json({ error: e.message });
-  
-  }
+//     res.json(locRes);
+//   } catch (e) {
+//     res.json({ error: e.message });
+//   }
+// });
 
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-}); 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- utils.js
+++ utils.js
@@ -14,15 +14,15 @@
 // }; 
 
 function mungeLocation(location) {
-    return {
-        [{
-            formatted_query: location.display_name,
-            latitude: location.lat,
-            longitude: location.lon
-        }]
-    };
+  return {
+
+    formatted_query: location[0].display_name,
+    latitude: location[0].lat,
+    longitude: location[0].lon
+
+  };
 }
 
 module.exports = {
-    mungeLocation
-}
+  mungeLocation
+};
