38bf868187da8d11b61f45cb0c478f84ad1679dc
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-10 16:02:08 -0800

    fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const request = require('superagent');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 const { mungeLocation, mungeWeather, mungeHiking, mungeYelp } = require('./utils.js');
 
 app.use(cors());
